fdbf42e6bb2172f76cac17e0745e532144ca7316
RahulSharma <rahulmlc2003@gmail.com>
2023-12-06 22:27:56 +0530

    added loading state till the content is reload

--- src/App.js
+++ src/App.js
@@ -8,35 +8,19 @@ import MoviesList from './components/MoviesList';
 function App() {
 
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
-  // const fetchMoviesHandler = () => {
-  //   fetch('https://swapi.dev/api/films')
-  //   .then((response) => {
-  //     return response.json();
-  //   })
-  //   .then((data) => {
-  //     const transformedMovies = data.results.map(movieData => {
-  //       return {
-  //         id: movieData.episode_id,
-  //         title: movieData.title,
-  //         openingText: movieData.opening_crawl,
-  //         releaseDate: movieData.release_date
-  //       }
-  //     })
-
-  //     setMovies( transformedMovies)
-  //   })
-
-  // }
-
-
+  
+  
   const fetchMoviesHandler = async () => {
     try{
+
+      setIsLoading(true)
       const response = await fetch('https://swapi.dev/api/films')
       const data = await response.json()
       
       const transformedMovies = data.results.map(movieData => {
-          return {
+        return {
             id: movieData.episode_id,
             title: movieData.title,
             openingText: movieData.opening_crawl,
@@ -45,18 +29,21 @@ function App() {
         })
   
         setMovies( transformedMovies)
-    }
+        setIsLoading(false)
+      }
     catch(error){
       console.log('Error fetching movies:', error)
     }
   }
-
+  
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler} className='btn'>Fetch Movies</button>
       </section>
       <section>
+        {isLoading && <p className='loading-text'>Loading...</p>}
+        {(!isLoading && movies.length===0) && <p className='loading-text'>Movie Not Found!</p>}
         <MoviesList movies={movies}/>
       </section>
     </React.Fragment>
@@ -64,3 +51,66 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// USING .then()  && .catch()
+
+// const fetchMoviesHandler = () => {
+//   fetch('https://swapi.dev/api/films')
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     const transformedMovies = data.results.map(movieData => {
+//       return {
+//         id: movieData.episode_id,
+//         title: movieData.title,
+//         openingText: movieData.opening_crawl,
+//         releaseDate: movieData.release_date
+//       }
+//     })
+
+//     setMovies( transformedMovies)
+//   })
+
+// }
+
+
+
+
+
+
+
+// USING async-await
+
+// const fetchMoviesHandler = async () => {
+//   try{
+//     const response = await fetch('https://swapi.dev/api/films')
+//     const data = await response.json()
+    
+//     const transformedMovies = data.results.map(movieData => {
+//       return {
+//           id: movieData.episode_id,
+//           title: movieData.title,
+//           openingText: movieData.opening_crawl,
+//           releaseDate: movieData.release_date
+//         }
+//       })
+
+//       setMovies( transformedMovies)
+//     }
+//   catch(error){
+//     console.log('Error fetching movies:', error)
+//   }
+// }
